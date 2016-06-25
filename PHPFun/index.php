3294c17a91f39528a73506d46b8002244edb3034
hilbertthedog <kayers20@gmail.com>
2016-06-25 00:33:39 -0500

    First commit

---
+++ PHPFun/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body bgcolor="#E6E6FA">
+          <?php
+           
+            $myArray=array ('Hilbert','Copernicus','Prince');
+            echo "<br />"."What are the names of Kim's pets?"."<br />";
+            foreach ($myArray as $item){
+                echo "<br />".$item."<br />";
+            }
+            define ('SITELINK', 'http://www.stackoverflow.com');
+
+        ?>
+        <img src="picture.jpg" width="250" height="175" title="Logo of a company" alt="Logo of a company">
+        <?php
+        echo "<br />"
+        ?>
+<a href="<?php echo SITELINK; ?>">Go to home!</a>
+    </body>
+</html>
